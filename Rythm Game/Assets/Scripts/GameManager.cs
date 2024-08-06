ebd73381525179206f1946421efe60b9530e7d78
almixmendez <almendezb2211@gmail.com>
2024-08-05 22:31:24 -0300

    Back button en RESULTS

--- Rythm Game/Assets/Scripts/GameManager.cs
+++ Rythm Game/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -157,5 +158,10 @@ public class GameManager : MonoBehaviour
         multiText.text = "Multiplier: x" + currentMultiplier;
         
         missedHits++;
+    }
+
+    public void GoBackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
